4d6a869e57855c2441b07ff0f005cd77e89ac708
Rachel Gibbons <rachelegibbons@gmail.com>
2017-02-23 14:08:52 -0800

    add functionality to hide and show destinations based upon input

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,23 @@ $(document).ready(function(){
     var temperature = $("select#temperatureInput").val();
     var favoriteDestination = $("input#favoriteDestinationInput").val();
 
+    if (type === "relax" && temperature === "hot") {
+      $("form").hide();
+      $("#hawaii").show();
+    } else if (type === "sightSee" && temperature === "hot" || "cold") {
+      $("form").hide();
+      $("#philadelphia").show();
+    } else if (type === "beActive" && temperature === "cold") {
+      $("form").hide();
+      $("#mtBachelor").show();
+    }
+
+    /*
     console.log(name);
     console.log(age);
     console.log(type);
     console.log(temperature);
-    console.log(favoriteDestination);
+    console.log(favoriteDestination); */
 
   });
 
